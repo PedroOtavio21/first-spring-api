73514fb51d27a10e14bc0b4139fd88ed1dad6b45
PedroOtavio21 <pedrootavio.bezerra14@gmail.com>
2026-06-23 20:00:03 -0300

    feat: adiciona primeiro commit do projeto

---
+++ src/main/java/com/example/first_spring_api/FirstSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringApiApplication.class, args);
+	}
+
+}
